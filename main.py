a3edcfe6a94fd4fb37a7b0eb6f6c3b9710e3be32
jaanerator <52489695+jaanerator@users.noreply.github.com>
2025-08-30 05:32:32 +0000

    refactor: Modulize Solver

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import time
 
 import numpy as np
@@ -9,40 +10,53 @@ from src.solver import get_all_combinations, compare
 def get_size(quiz):
     return len(quiz["h"]), len(quiz["v"])
 
+class NonogramsSolver:
+    def __init__(self, quiz):
+        self.quiz = deepcopy(quiz)
+        self.result = None
+        self.v = None
+        self.h = None
+        self._combinations_dict = None
+
+    def setup(self):
+        # get_size, get_all_combinations
+        self.result = np.full(get_size(self.quiz), "?", dtype=str)
+        self.v, self.h = self.result.shape
+        self._combinations_dict = {"v": [], "h": []}
+        for i in range(self.v):
+            combs = get_all_combinations(self.quiz["v"][i], self.h)
+            self._combinations_dict["v"].append(combs)
+        for i in range(self.h):
+            combs = get_all_combinations(self.quiz["h"][i], self.v)
+            self._combinations_dict["h"].append(combs)
+
+    def solve(self):
+        n_unknown = self.v * self.h
+        while True:
+            for i in range(self.v):
+                combs = self._combinations_dict["v"][i]
+                now_pattern = "".join(list(self.result[:, i]))
+                new_pattern = compare(combs, now_pattern)
+                self.result[:, i] = list(new_pattern)
+            for i in range(self.h):
+                combs = self._combinations_dict["h"][i]
+                now_pattern = "".join(list(self.result[i, :]))
+                new_pattern = compare(combs, now_pattern)
+                self.result[i, :] = list(new_pattern)
+
+            n_unknown_now = np.sum(self.result == "?")
+            if n_unknown_now == n_unknown or n_unknown_now < 1:
+                break
+            n_unknown = n_unknown_now
+
+
 if __name__ == "__main__":
     start = time.time()
 
-    result = np.full(get_size(QUIZ_SAMPLE), "?", dtype=str)
-    v, h = result.shape
-    n_unknown = v * h
+    solver = NonogramsSolver(QUIZ_SAMPLE)
+    solver.setup()
+    solver.solve()
 
-    combinations_dict = {"v": [], "h": []}
-    for i in range(v):
-        combs = get_all_combinations(QUIZ_SAMPLE["v"][i], h)
-        combinations_dict["v"].append(combs)
-    for i in range(h):
-        combs = get_all_combinations(QUIZ_SAMPLE["h"][i], v)
-        combinations_dict["h"].append(combs)
-
-    print(n_unknown)
-    while True:
-        for i in range(v):
-            combs = combinations_dict["v"][i]
-            now_pattern = "".join(list(result[:, i]))
-            new_pattern = compare(combs, now_pattern)
-            result[:, i] = list(new_pattern)
-        for i in range(h):
-            combs = combinations_dict["h"][i]
-            now_pattern = "".join(list(result[i, :]))
-            new_pattern = compare(combs, now_pattern)
-            result[i, :] = list(new_pattern)
-
-        n_unknown_now = np.sum(result == "?")
-        print(n_unknown_now)
-        if n_unknown_now == n_unknown or n_unknown_now < 1:
-            break
-        n_unknown = n_unknown_now
-
-    print("\n".join(["".join(list(elem)) for elem in list(result)]))
+    print("\n".join(["".join(list(elem)) for elem in list(solver.result)]))
     end = time.time()
     print(f"Elapsed: {end - start}")
